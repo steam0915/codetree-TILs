32c436243d2cdc9dc1602a5525802a1fd75839d6
steam0915 <166728864+steam0915@users.noreply.github.com>
2024-04-14 22:21:07 +0900

    Title: 황금비율 토스트, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240414/황금비율 토스트/golden-toast.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <list>
+using namespace std;
+int n, m;
+list<char> l;
+int main() {
+  cin >> n >> m;
+  for (int i = 0; i < n; i++) {
+    char a;
+    cin >> a;
+    l.push_back(a);
+  }
+
+  list<char>::iterator it;
+  it = l.end();
+  for (int i = 1; i <= m; i++) {
+    char command;
+    cin >> command;
+
+    if (command == 'L')
+      it--;
+
+    else if (command == 'R')
+      it++;
+
+    else if (command == 'D') {
+      it = l.erase(it); // 원소 'a'를 제거합니다.
+      cout << *it << endl;
+    }
+
+    else if (command == 'P') {
+      char a;
+      cin >> a;
+      l.insert(it, a);
+    }
+  }
+  for (it = l.begin(); it != l.end(); it++)
+    cout << *it;
+  return 0;
+}
