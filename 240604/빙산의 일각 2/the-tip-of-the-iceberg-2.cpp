032380d55825ff9269bed0ec7cd87e1f2a475ff7
steam0915 <166728864+steam0915@users.noreply.github.com>
2024-06-04 22:38:05 +0900

    Title: 빙산의 일각 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240604/빙산의 일각 2/the-tip-of-the-iceberg-2.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int n, ans = -1;
+int arr[105];
+int main() {
+  cin >> n;
+  for (int i = 1; i <= n; i++)
+    cin >> arr[i];
+
+  for (int i = 1; i <= 100; i++) {
+    int start = 1, end = 1, cnt = 0;
+    while (end <= n) {
+
+      bool xx = false;
+      while (start <= n) {
+        if (arr[end] - i > 0) {
+          xx = true;
+          end++;
+        } else
+          break;
+      }
+
+      if (!xx)
+        start++;
+      else
+        cnt++;
+
+      if (end < start)
+        end = start;
+    }
+    ans = max(ans, cnt);
+  }
+  cout << ans;
+  return 0;
+}
