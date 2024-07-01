f849b618baacb0272f120353a981cad4c6d10178
steam0915 <166728864+steam0915@users.noreply.github.com>
2024-07-01 23:45:29 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240701/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, ans = 1e9;
+pair<int, int> arr[105];
+int main() {
+  cin >> n;
+  for (int i = 1; i <= n; i++)
+    cin >> arr[i].first >> arr[i].second;
+
+  for (int i = 1; i <= n; i++) {
+    int max_x = 0, min_x = 40001, max_y = 0, min_y = 40001;
+    for (int j = 1; j <= n; j++) {
+      if (i == j)
+        continue;
+
+      max_x = max(max_x, arr[j].first), min_x = min(min_x, arr[j].first),
+      max_y = max(max_y, arr[j].second), min_y = min(min_y, arr[j].second);
+    }
+    ans = min(ans, (max_x - min_x) * (max_y - min_y));
+  }
+
+  cout << ans;
+  return 0;
+}
