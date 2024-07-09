460163b0e76f3484491f17d12f5e138ff4692309
steam0915 <166728864+steam0915@users.noreply.github.com>
2024-07-09 22:36:20 +0900

    Title: 수정 수집하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240709/수정 수집하기/collect-crystals.cpp
+++ 240709/수정 수집하기/collect-crystals.cpp
@@ -7,15 +7,6 @@ int n, k, ans;
 int arr[1005];
 int dp[1005][25][2];
 
-void Init() {
-  for (int i = 0; i <= n; i++)
-    for (int j = 0; j <= k; j++)
-      for (int pos = 0; pos <= 1; pos++)
-        dp[i][j][pos] = -1;
-
-  dp[0][0][0] = 0;
-}
-
 int main() {
   cin >> n >> k;
   for (int i = 1; i <= n; i++) {
@@ -24,14 +15,9 @@ int main() {
     (dir == 'L') ? arr[i] = 0 : arr[i] = 1;
   }
 
-  Init();
-
   for (int i = 0; i < n; i++) {
     for (int j = 0; j <= k; j++) {
       for (int pos = 0; pos <= 1; pos++) {
-        if (dp[i][j][pos] == -1)
-          continue;
-
         if (j < k)
           dp[i + 1][j + 1][!pos] = max(dp[i + 1][j + 1][!pos],
                                        dp[i][j][pos] + ((!pos) == arr[i + 1]));
