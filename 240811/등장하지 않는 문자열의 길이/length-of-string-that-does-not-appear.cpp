94f0a4a83309ac946782f1b3f7519ae1b14fb93f
steam0915 <166728864+steam0915@users.noreply.github.com>
2024-08-11 23:22:58 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240811/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.cpp
+++ 240811/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.cpp
@@ -10,23 +10,27 @@ int main() {
   cin >> str;
 
   for (int len = 1; len <= n; len++) {
+    bool sat = true;
     for (int i = 0; i < n - len + 1; i++) {
-      
+
       int cnt = 0;
       string min_str = str.substr(i, len);
-      
+
       for (int j = 0; j < n - len + 1; j++) {
+
         string s = str.substr(j, len);
         if (min_str == s)
-          cnt++;    
+          cnt++;
       }
-      if (cnt >= 2)
+      if (cnt >= 2) {
+        sat = false;
         break;
-
-      else {
-        cout << len;
-        return 0;
       }
+    }
+
+    if (sat) {
+      cout << len;
+      return 0;
     }
   }
   return 0;
