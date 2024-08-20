defd222a75025f6b3b48e2f3bc2805a72c3c282c
steam0915 <166728864+steam0915@users.noreply.github.com>
2024-08-20 22:27:59 +0900

    Title: 개인정보, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240820/개인정보/personal-info.cpp
+++ 240820/개인정보/personal-info.cpp
@@ -8,29 +8,31 @@ class Data {
 public:
   string name;
   int height;
-  int weight;
+  double weight;
 };
 
 Data arr[5];
 
-bool cmp1(const Data &a, const Data &b) {
-  return a.name < b.name;
-}
+bool cmp1(const Data &a, const Data &b) { return a.name < b.name; }
 
 bool cmp2(const Data &a, const Data &b) { return a.height > b.height; }
 
 int main() {
-  for (int i = 0; i < 5; i++) 
+  for (int i = 0; i < 5; i++)
     cin >> arr[i].name >> arr[i].height >> arr[i].weight;
 
-  sort(arr, arr + 5, cmp1);
+  cout << fixed;
+  cout.precision(1);
 
+  sort(arr, arr + 5, cmp1);
+  cout << "name\n";
   for (int i = 0; i < 5; i++)
-    cout << arr[i].name << " "<< arr[i].height << " "<<arr[i].weight;
+    cout << arr[i].name << " " << arr[i].height << " " << arr[i].weight << endl;
 
   sort(arr, arr + 5, cmp2);
-
+  cout << "\nheight\n";
   for (int i = 0; i < 5; i++)
-    cout << arr[i].name << " "<< arr[i].height << " "<<arr[i].weight;
+    cout << arr[i].name << " " << arr[i].height << " " << arr[i].weight << endl;
+
   return 0;
 }
