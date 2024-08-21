fcc03fac1d7af786d574c4a65884da7b64ef40ad
steam0915 <166728864+steam0915@users.noreply.github.com>
2024-08-21 23:00:29 +0900

    Title: 독서실의 거리두기 5, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240821/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
+++ 240821/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
@@ -13,13 +13,16 @@ int main() {
   for (int i = 0; i < n; i++) {
     if (arr[i])
       continue;
+
+    arr[i] = 1;
     int min_dis = 1e9;
-    for (int j = 0; j < n; j++) {
-      if (arr[j])
-        min_dis = min(min_dis, abs(i - j));
-    }
+    for (int j = 0; j < n; j++)
+      for (int k = j + 1; k < n; k++)
+        if (arr[k] && arr[j])
+          min_dis = min(min_dis, abs(j - k));
 
     ans = max(ans, min_dis);
+    arr[i] = 0;
   }
 
   cout << ans;
