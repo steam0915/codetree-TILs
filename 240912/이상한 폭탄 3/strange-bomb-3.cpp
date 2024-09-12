0f41ee204b13cf20c90c2dc40011245243904de3
steam0915 <166728864+steam0915@users.noreply.github.com>
2024-09-12 22:49:59 +0900

    Title: 이상한 폭탄 3, Time: 86ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240912/이상한 폭탄 3/strange-bomb-3.cpp
+++ 240912/이상한 폭탄 3/strange-bomb-3.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int n, k, ans;
+int n, k, ans, ans_num;
 int arr[100];
 int main() {
   cin >> n >> k;
@@ -13,17 +13,26 @@ int main() {
 
   for (int num = 0; num <= 1000000; num++) {
     int cnt = 0;
+    bool visit[100] = {};
+
     for (int i = 0; i < n; i++) {
       if (arr[i] == num) {
+        visit[i] = true;
         for (int j = 1; j <= k; j++) {
-          if (i + j < n && arr[i + j] == arr[i]) {
-            cnt++;
-            break;
-          }
+          if (i + j < n && arr[i + j] == arr[i])
+            visit[i + j] = true;
         }
       }
     }
-    ans = max(ans, cnt);
+
+    for (int i = 0; i < n; i++)
+      if (visit[i])
+        cnt++;
+
+    if (cnt > ans_num) {
+      ans_num = cnt;
+      ans = num;
+    }
   }
 
   cout << ans;
