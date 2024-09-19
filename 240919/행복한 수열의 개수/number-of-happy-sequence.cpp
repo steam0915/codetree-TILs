754798b209819abe1ee3bac14a0a4eeab95c2e72
steam0915 <166728864+steam0915@users.noreply.github.com>
2024-09-19 23:43:14 +0900

    Title: 행복한 수열의 개수, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240919/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 240919/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -5,6 +5,15 @@ using namespace std;
 
 int n, m, ans;
 int arr[105][105];
+
+bool Is_Same(vector<int> v, int start_point) {
+  for (int i = 0; i < m - 1; i++)
+    if (v[start_point + i] != v[start_point + i + 1])
+      return false;
+
+  return true;
+}
+
 int main() {
   cin >> n >> m;
   for (int i = 0; i < n; i++)
@@ -17,17 +26,11 @@ int main() {
     for (int j = 0; j < n; j++)
       v.push_back(arr[i][j]);
 
-    for (int j = 0; j < n - m + 1; j++) {
-      bool Same = true;
-      for (int k = 0; k < m - 1; k++)
-        if (v[j + k] != v[j + k + 1])
-          Same = false;
-
-      if (Same) {
+    for (int j = 0; j < n - m + 1; j++)
+      if (Is_Same(v, j)) {
         ans++;
         break;
       }
-    }
   }
 
   for (int i = 0; i < n; i++) {
@@ -36,17 +39,11 @@ int main() {
     for (int j = 0; j < n; j++)
       v.push_back(arr[j][i]);
 
-    for (int j = 0; j < n - m + 1; j++) {
-      bool Same = true;
-      for (int k = 0; k < m - 1; k++)
-        if (v[j + k] != v[j + k + 1])
-          Same = false;
-
-      if (Same) {
+    for (int j = 0; j < n - m + 1; j++)
+      if (Is_Same(v, j)) {
         ans++;
         break;
       }
-    }
   }
 
   cout << ans;
