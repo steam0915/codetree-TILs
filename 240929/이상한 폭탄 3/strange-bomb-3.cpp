766aa2322b535b3119a092e7566d842e55c80303
steam0915 <166728864+steam0915@users.noreply.github.com>
2024-09-29 22:24:07 +0900

    Title: 이상한 폭탄 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240929/이상한 폭탄 3/strange-bomb-3.cpp
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, k;
+pair<int, int> arr[100];
+pair<int, int> ans = {0, 0};
+bool visit[100];
+
+void Can_Exposed(int idx) {
+  if (idx + 1 < n && arr[idx].first == arr[idx + 1].first &&
+      arr[idx + 1].second - arr[idx].second <= k)
+    visit[idx] = visit[idx + 1] = true;
+}
+
+int main() {
+  cin >> n >> k;
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i].first;
+    arr[i].second = i;
+  }
+
+  sort(arr, arr + n);
+
+  int i = 0, j = 0;
+
+  while (i < n) {
+    int cnt = 0;
+    
+    while (j < n && arr[i].first == arr[j].first) {
+      Can_Exposed(j);
+      j++;
+    }
+    
+    for (int x = i; x < j; x++)
+      if (visit[x])
+        cnt++;
+
+    if (cnt != 0 && cnt >= ans.second)
+      ans = {arr[i].first, cnt};
+
+    i = j;
+  }
+
+  cout << ans.first;
+  return 0;
+}
